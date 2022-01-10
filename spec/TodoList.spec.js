2b4556e0b27efec6966d6d9cbac17a7b9ed0880a
Nico Picchio <nicolapicchio@gmail.com>
2022-01-10 20:18:47 +0000

    initial commit

--- spec/TodoList.spec.js
+++ spec/TodoList.spec.js
@@ -15,8 +15,93 @@ describe("TodoList", () => {
       status: "incomplete"
     }
     // execute
-    const result = todoList.create("turn the heating on!")
+    const result = todoList.createItem("turn the heating on!")
     // verify
     expect(result).toEqual(expected)
   })
+
+  it("get all todo items", () => {
+    const expected = [
+    {
+      id: 1,
+      text: "turn the heating on!",
+      status: "incomplete"
+    },
+    {
+      id: 2,
+      text: "meal prep",
+      status: "incomplete"
+    }
+  ]
+    todoList.createItem('turn the heating on!')
+    todoList.createItem('meal prep')
+    const result = todoList.fetchFullList()
+    expect(result).toEqual(expected)
+  })
+
+  it("set a todo completed by its ID", () => {
+    // set up
+    const expected = [
+      {
+        id: 1,
+        text: "turn the heating on!",
+        status: "complete"
+      },
+      {
+        id: 2,
+        text: "meal prep",
+        status: "incomplete"
+      }
+    ]
+    // execute
+    todoList.createItem('turn the heating on!')
+    todoList.createItem('meal prep')
+    const result = todoList.setCompletedByID(1)
+    // verify
+    expect(result).toEqual(expected)
+  })
+
+  it("return only incomplete todos", () => {
+    // set up
+    const expected = [
+      {
+        id: 1,
+        text: "meal prep",
+        status: "incomplete"
+      },
+      {
+        id: 2,
+        text: "play piano",
+        status: "incomplete"
+      }
+    ]
+    // execute
+    todoList.createItem('meal prep')
+    todoList.createItem('play piano')
+    todoList.createItem('code')
+    todoList.setCompletedByID(3)
+    const result = todoList.returnIncomplete()
+    // verify
+    expect(result).toEqual(expected)
+  })
+
+  it("return only complete todos", () => {
+    // set up
+    const expected = [
+      {
+        id: 3,
+        text: "code",
+        status: "complete"
+      }
+    ]
+    // execute
+    todoList.createItem('meal prep')
+    todoList.createItem('play piano')
+    todoList.createItem('code')
+    todoList.setCompletedByID(3)
+    const result = todoList.returnComplete()
+    // verify
+    expect(result).toEqual(expected)
+  })
+
 })
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,65 @@
 class TodoList {
+    constructor() {
+        this.todoList = [];
+    }
 
+    createItem(text) {
+        const todoItem = {
+            id: this.todoList.length+1,
+            text: text,
+            status: 'incomplete'
+        }
+        this.todoList.push(todoItem);
+        return todoItem
+    }
+
+    fetchFullList() {
+        return this.todoList
+    }
+
+    setCompletedByID(id) {
+        this.todoList[id-1].status = 'complete'
+        return this.todoList
+    }
+
+    returnIncomplete() {
+        const output = []
+        for (let i = 0; i < this.todoList.length; i++) {
+            if (this.todoList[i].status === 'incomplete') {
+                output.push(this.todoList[i])
+            }
+        }
+
+        return output
+    }
+
+    returnComplete() {
+        const output = []
+        for (let i = 0; i < this.todoList.length; i++) {
+            if (this.todoList[i].status === 'complete') {
+                output.push(this.todoList[i])
+            }
+        }
+        return output
+    }
+
+
+    // searchAndReturn(id) {
+    //     for (element of this.todoList) {
+    //         if (element.id === id) {
+    //             return element.text
+    //         }
+    //     }
+    //     return 'The item does not exist.'
+    // }
+
+    // removeItem(id) {
+    //     for (element of this.todoList) {
+    //         if (element.id === id) {
+    //             todoList.splice(element.id, 1)
+    //         }
+    //     }
+    // }
 }
 
 module.exports = TodoList
